07bd774352c4c8f0d0354487985842b3bf3bd1ca
SarkarTanmoy04 <58352262+SarkarTanmoy04@users.noreply.github.com>
2019-12-19 01:35:10 +0530

    Update create_folder.py

--- create_folder.py
+++ create_folder.py
@@ -1,5 +1,5 @@
 import os       #importing os module
-path=input("Please Enter any valid path: ")     #asking for a valid path, path will store into the valiable
+path=input("Please Enter any valid path: ")     #asking for a valid path, path will store into the variable
 os.chdir(path)  #will change directory to set the declared path
 folder=input("Please enter a name: ")   #asking for folder name, name will stored into the path
 os.mkdir(folder)    # command to create folder and passing the variable stored named as parameter
